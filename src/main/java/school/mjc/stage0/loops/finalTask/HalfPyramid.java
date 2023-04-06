751b2fdf67e837ec3bb0f898ba9b2b60a8eaf9dd
ZaycevSergey <77642306+ZaicevSergey@users.noreply.github.com>
2023-04-06 09:01:39 +0300

    solved

--- src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
@@ -1,8 +1,24 @@
 package school.mjc.stage0.loops.finalTask;
 
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class HalfPyramid {
     public void printHalfPyramid(int cathetusLength) {
+        if (cathetusLength <= 0) {
+            return;
+        }
 
+        for (int i = 1; i <= cathetusLength; i++) {
+            String line = IntStream.range(0, cathetusLength - i)
+                                  .mapToObj(j -> " ")
+                                  .collect(Collectors.joining())
+                          + IntStream.range(0, i)
+                                  .mapToObj(j -> "*")
+                                  .collect(Collectors.joining());
+            System.out.println(line);
+
+        }
     }
 
     public static void main(String[] args) {
